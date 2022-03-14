ad3157ac3e9da6dbe3c0c342c06d9c12c238808c
Ronald Pineda <ronystivens@gmail.com>
2022-03-14 23:16:25 +0100

    refactored request-validation error with delegation

--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,10 +1,18 @@
 import { ValidationError } from 'express-validator';
 
 export class RequestValidationError extends Error {
+  statusCode = 400;
+
   constructor(public errors: ValidationError[]) {
     super();
 
     //Because this class is extends from a built-in class 'Error'
     Object.setPrototypeOf(this, RequestValidationError.prototype);
+  }
+
+  serializeErrors() {
+    return this.errors.map(err => {
+      return { message: err.msg, field: err.param };
+    });
   }
 }
