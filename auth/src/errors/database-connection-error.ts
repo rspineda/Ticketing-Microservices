aade283adfec0f103b377b478feb6fbb11f9a99e
Ronald Pineda <ronystivens@gmail.com>
2022-03-14 23:09:52 +0100

    refactored db connection error with delegation

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,4 +1,5 @@
 export class DatabaseConnectionError extends Error {
+  statusCode = 500;
   reason = 'Error connecting to database';
 
   constructor() {
@@ -6,5 +7,11 @@ export class DatabaseConnectionError extends Error {
 
     //Because this class is extends from a built-in class 'Error'
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+
+  serializeErrors() {
+    return [
+      { message: this.reason }
+    ];
   }
 }
