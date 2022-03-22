8b4166b2e0b09e62b0791fd896efc6449785eee7
Ronald Pineda <ronystivens@gmail.com>
2022-03-23 00:49:08 +0100

    applied UserDocument TS typing

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,12 +1,20 @@
 import mongoose from 'mongoose';
 
+//Properties that are required to create a new User
 interface UserAttributes {
   email: string;
   password: string;
 }
 
-interface UserModel extends mongoose.Model<any> {
-  build(attributes: UserAttributes): any;
+//Properties that a User Model has
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attributes: UserAttributes): UserDoc;
+}
+
+//Properties that a User Document has
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
 }
 
 const userSchema = new mongoose.Schema({
@@ -24,13 +32,14 @@ userSchema.statics.build = (attributes: UserAttributes) => {
   return new User(attributes);
 };
 
-const User = mongoose.model<any, UserModel>('User', userSchema);
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
 
 /* 
-User.build({
+const user = User.build({
   email: 'axsd@gmail.com',
   password: '12345'
 });
 */
+
 
 export { User };
